901eacb5d5c98be021d350321a720571696c4d80
Javier Gomez <javier.gov@outlook.es>
2023-08-17 00:05:33 -0600

    Ejemplos de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'MyStore';
+  title = 'Javier';
+  age = 30;
+  btnDisabled = false;
+
+  person = {
+    name: "Javier objeto person",
+    age: 18
+  }
+
+  toogleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
 }
